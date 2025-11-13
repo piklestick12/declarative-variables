920d409b3a39d96797f225dd9dc0a33de4433007
picklestick12 <jhndvdarevalo@gmail.com>
2025-11-14 07:36:44 +0800

    create a declarative-variables file in c-project

---
+++ main.c
@@ -0,0 +1,16 @@
+// AREVALO, JOHN DAVID A. DIT 1-2
+#include <stdio.h>
+int main()
+{
+
+    int num1 = 10;
+    float num2 = 5.5;
+    char grade = 'A';
+
+    printf("Initial Values");
+    printf("\nnum1 = %d\n", num1);
+    printf("num2 = %f\n", num2);
+    printf("grade = %c\n\n", grade);
+
+    return 0;
+}
